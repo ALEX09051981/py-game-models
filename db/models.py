89a0ff1b1677bdd1afda3d0df165979700e77a4b
Alexey <alopar632@gmail.com>
2025-06-19 14:34:10 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -5,16 +5,20 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skills",
+    )
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
@@ -22,7 +26,7 @@ class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
@@ -30,7 +34,11 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="players",
+    )
     guild = models.ForeignKey(
         Guild,
         on_delete=models.SET_NULL,
@@ -40,5 +48,5 @@ class Player(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.nickname
